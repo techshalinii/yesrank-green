892784eef1de447fa349edf848cbc541be1c3f59
techshalinii <shalinikumari9142@gmail.com>
2024-10-22 20:30:22 +0530

    change images

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,11 +1,14 @@
 'use client';
 import cogImage from '@/assets/cog.png'
-import cylinderImage from '@/assets/cylinder.png';
-import noodleImage from '@/assets/noodle.png'
+import cylinderImage from '@/assets/cylinder1.png';
+import noodleImage from '@/assets/half.png'
 import {motion, useScroll, useTransform, useMotionValueEvent} from 'framer-motion'
 import {useRef} from 'react';
 
 export const Hero = () => {
+
+
+  
   const heroRef = useRef(null);
   const{scrollYProgress} = useScroll({
     target:heroRef,
@@ -14,11 +17,11 @@ export const Hero = () => {
   const translateY = useTransform(scrollYProgress, [0,1], [150, -150]);
 
   return (
-    <div ref={heroRef} className='pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip'>
+    <div id='home' ref={heroRef} className='pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#06D6A0,#EAEEFE_100%)] overflow-x-clip'>
       <div className="container">
         <div className='md:flex items-center'>
         <div className='md:w-[478px]'>
-          <h1 className="text-5xl md:text-7xl font-bold tracking-tight bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">Accelerate Your Business Growth</h1>
+          <h1 className="text-5xl md:text-7xl font-bold tracking-tight bg-gradient-to-b from-black to-[#02382A] text-transparent bg-clip-text mt-6">Accelerate Your Business Growth</h1>
           <p className="text-xl text-[#010D3E] tracking-tight mt-6">
           Partner with Yesrank for innovative digital marketing strategies that drive exceptional ROI and measurable success
           </p>
@@ -53,7 +56,7 @@ export const Hero = () => {
           src={noodleImage.src} 
           alt='img' 
           width={220} 
-          className='hidden lg:block absolute top-[524px] left-[448px] rotate-[30deg]'
+          className='hidden lg:block absolute top-[640px] left-[448px] rotate-[31deg]'
           style={{
             rotate:30,
             translateY: translateY}} 
