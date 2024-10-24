7fa1d8ec6717753c78f1eb479e3ac898f65d7719
techshalinii <shalinikumari9142@gmail.com>
2024-10-25 00:01:31 +0530

    removed header and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,22 @@
 import { CallToAction } from "@/sections/CallToAction";
-import { Footer } from "@/sections/Footer";
-import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
 import { LogoTicker } from "@/sections/LogoTicker";
 import { Testimonials } from "@/sections/Testimonials";
-import {Service} from "@/sections/Service"
+import Services from "@/sections/Service";
 import { WhyUs } from "@/sections/WhyUs";
 import { Blog } from "@/sections/Blog";
 
 export default function Home() {
   return (
     <>
-      <Header />
       <Hero />
       <LogoTicker />
-      <Service />
+      <Services />
       <WhyUs />
       <Testimonials />
       <Blog />
       < CallToAction />
-      <Footer />
+      
     </>
   );
 }
